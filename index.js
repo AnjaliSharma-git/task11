4839485070cd41e4903f5c0e616fc0bacf6c8108
Anjali Sharma <anjali.sharma060202@gmail.com>
2024-11-05 17:48:42 +0530

    abc

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGO_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true 
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+app.use(express.json());
+
 app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-        app.listen(3000, () => {
-            console.log('Server is running on port 3000');
-        });
-    })
-    .catch(err => console.error(err));
+app.get('/', (req, res) => {
+    res.send('Welcome to the User Authentication API');
+});
+
+const PORT = process.env.PORT || 5000; 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
